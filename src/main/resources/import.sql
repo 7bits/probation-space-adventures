74e741fabbff65fa3eaf0cfd9fd9e6782ccb4b4f
TuMyp <bojarinovdenis@gmail.com>
2013-07-30 13:01:58 +0700

    add entry in table event

--- src/main/resources/import.sql
+++ src/main/resources/import.sql
@@ -1,1 +1,8 @@
+--Subscription table insert
 insert into subscription(id, email) values(1,"test10@mail.ru");
+
+--Event table insert
+insert into event(id, name, description, date, eye) values(1,"test1","test description 1",1000,1);
+insert into event(id, name, description, date, eye) values(2,"test2","test description 2",2000,1);
+insert into event(id, name, description, date, eye) values(3,"test3","test description 3",3000,1);
+insert into event(id, name, description, date, eye) values(4,"test4","test description 4",4000,1);
