47b4baf1794d6b187587a2bafe8478f9d4130100
openlex <openlexx@gmail.com>
2013-08-12 12:52:48 +0700

    notification

--- src/main/webapp/script/form-actions.js
+++ src/main/webapp/script/form-actions.js
@@ -1,24 +1,29 @@
   // prepare the form when the DOM is ready
-  $(document).ready(function() {
-      var options = {
-          success:       showResponse  // post-submit callback
-      };
-      $('#subscribe-form').ajaxForm(options);
-  });
+$(document).ready(function() {
+  var options = {
+      success:       showResponse  // post-submit callback
+  };
+  $('#subscribe-form').ajaxForm(options);
+});
 
 
-  // post-submit callback
-  function showResponse(responseText, statusText, xhr, form)  {
-        console.log(responseText);
-     if (responseText.length==0)  {
-          alert("Спасибо за подписку")
-           $('#subscribe-form-div').empty();
+// post-submit callback
+function showResponse(responseText, statusText, xhr, form)  {
+    console.log(responseText);
+ if (responseText.length==0)  {
+  //  body.insertBefore($('<div id="subscribe-notification"> ffffffff </div>'), refNode)
+    $('<div id="subscribe-notification"> Подписка прошла успешно </div>').insertBefore('body');
+    $("#subscribe-notification").css("visibility", "visible").fadeTo('fast', '1');
 
-     } else {
-            $('#subscribe-form-div').empty();
-             $('#subscribe-form-div').html(responseText);
-     }
-      $("#dark-bg").css("visibility", "hidden");
-  }
+    $('#subscribe-form-div').empty();
+    $("#dark-bg").css("visibility", "hidden");
+
+
+
+ } else {
+    $('#subscribe-form-div').empty();
+    $('#subscribe-form-div').html(responseText);
+ }
+}
 
 
